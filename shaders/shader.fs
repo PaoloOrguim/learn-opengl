268895edbb6291b50b2d011ba1d1af9e2cfa5ba4
PaoloOrguim <paolowow00@gmail.com>
2026-01-30 15:42:41 -0300

    Fragment color equal to vertex position exercise.

--- shaders/shader.fs
+++ shaders/shader.fs
@@ -1,9 +1,13 @@
 #version 330 core
 out vec4 FragColor;
 
-in vec3 ourColor;
+//in vec3 ourColor;
+in vec3 ourPosition;
 
 void main()
 {
-    FragColor = vec4(ourColor, 1.0f);
+    //FragColor = vec4(ourColor, 1.0f);
+    FragColor = vec4(ourPosition, 1.0); // Position is interpolated giving the triangle the different colors
+    // Bottom-left coordinate of the triangle is (-0.5f, -0.5f, 0.0f), negative values are clamped to 0.0f
+    // The values only start becoming positive (therefore not black) after the center of the sides.
 }
