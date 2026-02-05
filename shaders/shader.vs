bc3c819e0561d308fbb5dd2635289fe639008f30
PaoloOrguim <paolowow00@gmail.com>
2026-02-05 18:26:47 -0300

    Shader class update. Small clean ups

--- shaders/shader.vs
+++ shaders/shader.vs
@@ -7,10 +7,12 @@ out vec3 ourColor;
 out vec2 TexCoord;
 
 uniform mat4 model;
+uniform mat4 view;
+uniform mat4 projection;
 
 void main()
 {
-	gl_Position = model * vec4(aPos, 1.0);
+	gl_Position = projection * view * model * vec4(aPos, 1.0);	// Matrix multiplication happens right to left model -> view -> projection
 	ourColor = aColor;
 	TexCoord = aTexCoord;
 }
--- src/main.cpp
+++ src/main.cpp
@@ -188,24 +188,32 @@ int main()
 
         // Going 3D
         // --------
+
+        // Model
         glm::mat4 model = glm::mat4(1.0f);  // Local Space -> World Space happens through the model matrix
         model = glm::rotate(model, glm::radians(-55.0f), glm::vec3(1.0f, 0.0f, 0.0f));
-
+        
+        // View
         glm::mat4 view = glm::mat4(1.0f);
         // note that we're translating the scene in the reverse direction of where we want to move
         view = glm::translate(view, glm::vec3(0.0f, 0.0f, -3.0f));
         // We multiply in the correct order: View * Model
-        model = view * model;
 
+        // Projection
         glm::mat4 projection;
         projection = glm::perspective(glm::radians(45.0f), 800.0f / 600.0f, 0.1f, 100.0f);
 
-        model = projection * model; // Remember: Projection * View * Model
 
         // get matrix's uniform location and set matrix
         ourShader.use();
         unsigned int modelLoc = glGetUniformLocation(ourShader.ID, "model");
         glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
+
+        unsigned int viewLoc = glGetUniformLocation(ourShader.ID, "view");
+        glUniformMatrix4fv(viewLoc, 1, GL_FALSE, &view[0][0]);
+        
+        unsigned int projectionLoc = glGetUniformLocation(ourShader.ID, "projection");
+        ourShader.setMat4("projection", projection);
 
         // set the texture mix value in the shader
         ourShader.setFloat("mixValue", mixValue);
