ef371cfc40e9b767b6abf33b188150e20fdca7a5
PaoloOrguim <paolowow00@gmail.com>
2026-02-10 19:52:12 -0300

    Added specular lighting. Not finished.

--- src/main.cpp
+++ src/main.cpp
@@ -194,6 +194,7 @@ int main()
         lightingShader.setVec3("objectColor", 1.0f, 0.5f, 0.31f);
         lightingShader.setVec3("lightColor",  1.0f, 1.0f, 1.0f);
         lightingShader.setVec3("lightPos", lightPos); // Can be done outside the render loop, for now
+        lightingShader.setVec3("viewPos", camera.Position);
 
         // view/projection transformations
         glm::mat4 projection = glm::perspective(glm::radians(camera.Zoom), (float)SCR_WIDTH / (float)SCR_HEIGHT, 0.1f, 100.0f);
